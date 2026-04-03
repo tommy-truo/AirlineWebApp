7dfe5d32811ff61a267ab6e884eb0ef79d3e8220
dex 🏂 <terry@MacBook-Pro-1893.local>
2026-04-03 17:22:59 -0500

    Configure server for local + production environments and improve sidebar UI styling

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ import authRouter from './src/routes/authRoutes.js';
 import pilotRouter from './src/routes/pilotRouter.js';
 
 const app = express();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5001;
 
 app.use(cors()); 
 
