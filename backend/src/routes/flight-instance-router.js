95563cd3efd158daa84cc427838153523bee9ec7
aya <99104777+ayadotexe@users.noreply.github.com>
2026-04-17 00:18:46 -0500

    Add updateFlight route to flight-instance-router

--- backend/src/routes/flight-instance-router.js
+++ backend/src/routes/flight-instance-router.js
@@ -10,5 +10,6 @@ router.get('/search', FlightController.findFlights);
 router.get('/dropdowns', FlightController.getDropdownData);
 router.post('/create', FlightController.createFlight);
 router.get('/all', FlightController.getAllFlights);
+router.put('/:id', FlightController.updateFlight);
 
 export default router;
