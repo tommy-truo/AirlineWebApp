a1fd66a38ab86b2c743b59b5001f9f1a55d94f7f
aya <99104777+ayadotexe@users.noreply.github.com>
2026-04-17 22:26:07 -0500

    Update import path for styles in EmployeeManagement

--- frontend/src/pages/EmployeeManagement.jsx
+++ frontend/src/pages/EmployeeManagement.jsx
@@ -1,7 +1,7 @@
 import axios from "axios"
 import { useState, useEffect } from 'react';
 import { Link } from "react-router-dom"
-import '../styles/styles.css';
+import '../components/styles.css';
 
 function EmployeeManagement() {
 
