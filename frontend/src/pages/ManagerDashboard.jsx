2815ec4c7282d711235c2406b8205f28fbfea187
aya <99104777+ayadotexe@users.noreply.github.com>
2026-04-19 12:27:05 -0500

    Refactor ManagerDashboard routes for employee management

--- frontend/src/pages/ManagerDashboard.jsx
+++ frontend/src/pages/ManagerDashboard.jsx
@@ -3,36 +3,36 @@ import { Routes, Route, Navigate } from "react-router-dom";
 import ManagerLayout from "../components/ManagerLayout";
 
 import ManagerHome from "./ManagerHome.jsx";
-import EmployeeAssignments from "./EmployeeAssignments.jsx";
-import EmployeeManagement from "./EmployeeManagement.jsx";
-import EmployeeRegisterForm from "./EmployeeRegisterForm.jsx";
-import EmployeeShiftRequests from "./EmployeeShiftRequests.jsx";
-import FlightManagement from "./FlightManagement.jsx";
-import CreateFlight from "./CreateFlight.jsx";
-import PayrollReports from "./PayrollReports.jsx";
-import TransactionHistory from "./TransactionHistory.jsx";
-import TransactionReports from "./TransactionReports.jsx";
-import FlightReports from "./FlightReports.jsx";
-import FlashSaleManagement from "./FlashSaleManagement.jsx";
-import CreateFlashSale from "./CreateFlashSale.jsx";
+import ManagerEmployeeAssignments from "./ManagerEmployeeAssignments.jsx";
+import ManagerEmployeeManagement from "./ManagerEmployeeManagement.jsx";
+import ManagerEmployeeRegisterForm from "./ManagerEmployeeRegisterForm.jsx";
+import ManagerEmployeeShiftRequests from "./ManagerEmployeeShiftRequests.jsx";
+import ManagerFlightManagement from "./ManagerFlightManagement.jsx";
+import ManagerCreateFlight from "./ManagerCreateFlight.jsx";
+import ManagerPayrollReports from "./ManagerPayrollReports.jsx";
+import ManagerTransactionHistory from "./ManagerTransactionHistory.jsx";
+import ManagerTransactionReports from "./ManagerTransactionReports.jsx";
+import ManagerFlightReports from "./ManagerFlightReports.jsx";
+import ManagerFlashSaleManagement from "./ManagerFlashSaleManagement.jsx";
+import ManagerCreateFlashSale from "./ManagerCreateFlashSale.jsx";
 
 function ManagerDashboard({ employeeId, onLogout }) {
   return (
     <ManagerLayout onLogout={onLogout}>
       <Routes>
         <Route path="/" element={<ManagerHome employeeId={employeeId} />} />
-        <Route path="/assignments" element={<EmployeeAssignments employeeId={employeeId} />} />
-        <Route path="/employee-directory" element={<EmployeeManagement employeeId={employeeId} />} />
-        <Route path="/employee-register" element={<EmployeeRegisterForm employeeId={employeeId} />} />
-        <Route path="/shift-requests" element={<EmployeeShiftRequests employeeId={employeeId} />} />
-        <Route path="/flights" element={<FlightManagement employeeId={employeeId} />} />
-        <Route path="/create-flight" element={<CreateFlight employeeId={employeeId} />} />
-        <Route path="/flight-reports" element={<FlightReports employeeId={employeeId} />} />
-        <Route path="/payroll-reports" element={<PayrollReports employeeId={employeeId} />} />
-        <Route path="/transaction-history" element={<TransactionHistory />} />
-        <Route path="/transaction-reports" element={<TransactionReports />} />
-        <Route path="/flash-sales" element={<FlashSaleManagement />} />
-        <Route path="/create-flash-sale" element={<CreateFlashSale />} />
+        <Route path="/assignments" element={<ManagerEmployeeAssignments employeeId={employeeId} />} />
+        <Route path="/employee-directory" element={<ManagerEmployeeManagement employeeId={employeeId} />} />
+        <Route path="/employee-register" element={<ManagerEmployeeRegisterForm employeeId={employeeId} />} />
+        <Route path="/shift-requests" element={<ManagerEmployeeShiftRequests employeeId={employeeId} />} />
+        <Route path="/flights" element={<ManagerFlightManagement employeeId={employeeId} />} />
+        <Route path="/create-flight" element={<ManagerCreateFlight employeeId={employeeId} />} />
+        <Route path="/flight-reports" element={<ManagerFlightReports employeeId={employeeId} />} />
+        <Route path="/payroll-reports" element={<ManagerPayrollReports employeeId={employeeId} />} />
+        <Route path="/transaction-history" element={<ManagerTransactionHistory />} />
+        <Route path="/transaction-reports" element={<ManagerTransactionReports />} />
+        <Route path="/flash-sales" element={<ManagerFlashSaleManagement />} />
+        <Route path="/create-flash-sale" element={<ManagerCreateFlashSale />} />
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </ManagerLayout>
