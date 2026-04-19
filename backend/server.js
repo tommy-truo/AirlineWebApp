44b2d8bb36e6fe1f7cabca297eef406d94b190e5
aya <99104777+ayadotexe@users.noreply.github.com>
2026-04-18 20:22:39 -0500

    Add new routers for employee and transaction APIs

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,13 @@ import bookingRouter from './src/routes/booking-router.js';
 import flightRouter from './src/routes/flight-instance-router.js';
 import authRouter from './src/routes/authRoutes.js';
 import pilotRouter from './src/routes/pilotRouter.js';
+// below added by aya
+import employeeRouter from './src/routes/employeeRouter.js';
+import transactionRouter from './src/routes/transactionRouter.js';
+import payrollRouter from './src/routes/payrollRouter.js';
+import shiftRequestRouter from './src/routes/shiftRequestRouter.js'
+import assignmentRouter from './src/routes/assignmentRouter.js'
+import flightReportRouter from './src/routes/flightReportRouter.js'
 
 const app = express();
 const PORT = process.env.PORT;
@@ -22,6 +29,13 @@ app.use('/api/bookings', bookingRouter);
 app.use('/api/flights', flightRouter)
 app.use('/api/auth', authRouter);
 app.use('/api/pilot', pilotRouter);
+// below added by aya
+app.use('/api/employees', employeeRouter);
+app.use('/api/transactions', transactionRouter);
+app.use('/api/payroll', payrollRouter);
+app.use('/api/requests', shiftRequestRouter)
+app.use('/api/assignments', assignmentRouter)
+app.use('/api/flight-reports', flightReportRouter)
 
 app.use((err, req, res, next) => {
     console.error("SERVER ERROR:", err);
