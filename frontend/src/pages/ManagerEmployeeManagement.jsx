cb8943f81e4c198275becf58f1d9959707c5e97e
aya <99104777+ayadotexe@users.noreply.github.com>
2026-04-19 13:34:28 -0500

    Update ManagerEmployeeManagement.jsx

--- frontend/src/pages/ManagerEmployeeManagement.jsx
+++ frontend/src/pages/ManagerEmployeeManagement.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 import { Link } from "react-router-dom";
-import '../components/styles.css';
+import "../components/ManagerStyles.css";
 
 function EmployeeManagement() {
   const [data, setData] = useState([]);
