2ee71eb3c19b019c583fc468d7cc959d8bebda63
Abhishek Singh <abhisheksingh@MacBookPro.attlocal.net>
2026-04-19 01:15:06 -0500

    addition to server file

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,7 @@ import bookingRouter from './src/routes/booking-router.js';
 import flightRouter from './src/routes/flight-instance-router.js';
 import authRouter from './src/routes/authRoutes.js';
 import pilotRouter from './src/routes/pilotRouter.js';
+import employeeRoutes from './src/routes/empDashRoutes.js';
 
 const app = express();
 const PORT = process.env.PORT;
@@ -22,6 +23,7 @@ app.use('/api/bookings', bookingRouter);
 app.use('/api/flights', flightRouter)
 app.use('/api/auth', authRouter);
 app.use('/api/pilot', pilotRouter);
+app.use('/api/employee', employeeRoutes);
 
 app.use((err, req, res, next) => {
     console.error("SERVER ERROR:", err);
