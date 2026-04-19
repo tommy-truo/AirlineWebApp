ca06715fc093e94887fe4c8cc55601d01fb626e4
aya <99104777+ayadotexe@users.noreply.github.com>
2026-04-19 13:33:06 -0500

    Update ManagerEmployeeAssignments.jsx

--- frontend/src/pages/ManagerEmployeeAssignments.jsx
+++ frontend/src/pages/ManagerEmployeeAssignments.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react'
-import '../components/styles.css'
+import "../components/ManagerStyles.css";
 
 function EmployeeAssignments() {
   const [assignments, setAssignments] = useState([])
